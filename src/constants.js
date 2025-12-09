ece3457ebcad47281152a158e93e41208e4eaf04
Student AppnaCollege <student@appnacollege.in>
2025-12-09 23:05:08 +0530

    Initial project commit

---
+++ src/constants.js
@@ -0,0 +1,179 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import pentagonspace_logo from './assets/company_logo/pentagonspace_logo.png';
+import destination_logo from './assets/company_logo/destination_logo.png';
+
+
+// Education Section Logo's
+import biet_logo from './assets/education_logo/biet_logo.png';
+import sirmv_logo from './assets/education_logo/sirmv_logo.png';
+import siddaganga_logo from './assets/education_logo/siddaganga_logo.png';
+
+// Project Section Logo's
+import finai from './assets/work_logo/financeai.png';
+import spam from './assets/work_logo/spamclass.png';
+import shop from './assets/work_logo/shopcart.png';
+import gym from './assets/work_logo/gym.png';
+import pcm from './assets/work_logo/placestat.png';
+import todo from './assets/work_logo/todoapp.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: pentagonspace_logo,
+    role: "Fullstack Developer Trainee",
+    company: "Pentagon Space",
+    date: "April 2025 - Present",
+    desc: "Working as a Fullstack Developer Trainee, building end-to-end web applications using the MERN stack along with TypeScript and Next.js. Developed responsive and interactive UIs with ReactJS and Tailwind CSS, implemented robust RESTful APIs using Node.js, and managed databases with MongoDB. Leveraged Redux for state management and explored Java-based backend frameworks like Spring and Spring Boot to create scalable, high-performance systems.",
+    skills: [
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      "Next Js",
+      "Spring",
+      "Springboot",
+    ],
+  },
+];
+
+
+  
+  export const education = [
+    {
+      id: 0,
+      img: SB_logo,
+      school: "Sharnbasva University",
+      date: "Nov 2021 - June 2025",
+      grade: "8.83 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Science and Engineering from Sharnbasva University, Kalaburagi. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at Sharnbasva University College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Engineering - BE (Computer Science)",
+    },
+    {
+      id: 1,
+      img: SB_logo,
+      school: "DoddAppa Appa PU College Kalaburagi",
+      date: "Sept 2019 - Aug 2021",
+      grade: "74.16%",
+      desc: "I completed my class 11 and 12 education from DoddAppa Appa Pu College , Kalaburagi, under the State board, where I studied Physics, Chemistry, and Mathematics (PCM) with Biology.",
+      degree: "Pre-University - PCMB",
+    },
+    {
+      id: 2,
+      img: GVM_logo,
+      school: "Gurukul Vidya Mandir",
+      date: "Apr 2018 - March 2019",
+      grade: "68.64%",
+      desc: "I completed my class 10 education from Gurukul Vidya Mandir School, Kalaburagi, under the State Board.",
+      degree: "State(X), Science with Computer Application",
+    },
+
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Shopping-Cart-J2EE",
+      description:
+      "Shopping-Cart-J2EE is a Java-based web application designed to demonstrate how a modern e-commerce shopping cart can be implemented using J2EE (Java 2 Enterprise Edition) technologies. The project focuses on providing a complete online shopping workflow—from browsing products to placing an order—using a scalable and modular multi-tier architecture..",
+
+      image: finai,
+      tags: ["","Git"],
+      github: "https://github.com/Vishalnimbal/Shopping-Cart-J2EE",
+      webapp: "https://github.com/Vishalnimbal/Shopping-Cart-J2EE",
+    },
+  ];
